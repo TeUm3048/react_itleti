1d5d4310c13e5c367548ea13333492b1da42bc66
TeUm3048 <teum3048@yandex.ru>
2022-11-14 23:10:34 +0300

    Add styles

--- pages/gallery.jsx
+++ pages/gallery.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { Button } from "../components/Button/Button"
 import Image from "next/image";
+import css from "../styles/Gallery.module.css";
 
 export default function GalletyPage() {
   const [content, setContent] = useState(null);
@@ -31,38 +32,46 @@ export default function GalletyPage() {
 
   }, [])
 
-  return (
-    <main>
-      <form onSubmit={handleClick}>
-        <Button type="submit">submit</Button>
-        {/* <input type="url" value={title} onChange={e => setTitle(e.currentTarget.title)} /> */}
 
-        <label >
-          Название картинки
-          <input type="text" value={titleImg} onChange={e => setTitleImg(e.currentTarget.value)} />
+  return (
+    <div className={css.wrapper}>
+      <form className={css.form} onSubmit={handleClick}>
+
+        <label className={css.form__item}>
+          <span className={css.form__item_name}>
+            Название картинки
+          </span>
+          <input className={css.form__item_input} type="text" value={titleImg} onChange={e => setTitleImg(e.currentTarget.value)} />
         </label>
-        <label >
-          Ссылка на картинку
-          <input type="url" value={thumbnailUrl} onChange={e => setThumbnailUrl(e.currentTarget.value)} />
+        <label className={css.form__item}>
+          <span className={css.form__item_name}>
+            Ссылка на картинку
+          </span>
+          <input className={css.form__item_input} type="url" value={thumbnailUrl} onChange={e => setThumbnailUrl(e.currentTarget.value)} />
         </label>
-        <label >
-          Ссылка на миниатюру картинки
-          <input type="url" value={url} onChange={e => setUrl(e.currentTarget.value)} />
+        <label className={css.form__item}>
+          <span className={css.form__item_name}>
+            Ссылка на миниатюру картинки
+          </span>
+          <input className={css.form__item_input} type="url" value={url} onChange={e => setUrl(e.currentTarget.value)} />
         </label>
-      </form >
-      <Button onClick={() => setPageIndex(p => p - 1)}>Prev</Button>
-      <span>{pageIndex + 1}</span>
-      <Button onClick={() => setPageIndex(p => p + 1)}>Next</Button>
-      <ul>
+        <Button type="submit">submit</Button>
+      </form>
+      <div className={css.paginator}>
+        <Button onClick={() => setPageIndex(p => p > 0 ? p - 1 : 0)}>Prev</Button>
+        <span>{pageIndex + 1}</span>
+        <Button onClick={() => setPageIndex(p => p + 1)}>Next</Button>
+      </div>
+      <ul className={css.gallery}>
         {content?.slice(pageIndex * 12, (pageIndex + 1) * 12).map((el => (
-          <li key={el.id}>
-            <img src={el.thumbnailUrl} alt='' />
-            <span>{el.title}</span>
+          <li key={el.id} className={css.gallery__item}>
+            <img className={css.gallery__img} src={el.url} alt='' />
+            <span className={css.gallery__title}>{el.title}</span>
           </li>
 
         )))}
       </ul>
-    </main>
+    </div>
 
   )
 }
